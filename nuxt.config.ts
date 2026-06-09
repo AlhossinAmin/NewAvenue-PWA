6584010b18499f24138c6842988c93b1153320fc
AlhoussinAmin <Alhoussin@34ml.com>
2026-06-09 12:38:34 +0300

    feat: setup PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,60 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@vite-pwa/nuxt"],
   css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      link: [{ rel: "apple-touch-icon", href: "/apple-touch-icon.png" }],
+      meta: [
+        { name: "apple-mobile-web-app-capable", content: "yes" },
+        { name: "apple-mobile-web-app-status-bar-style", content: "default" },
+        { name: "theme-color", content: "#ffffff" },
+      ],
+    },
+  },
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Avenu",
+      short_name: "Avenu",
+      description: "Avenu PWA",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      display: "standalone",
+      orientation: "portrait",
+      start_url: "/",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: "/",
+      globPatterns: ["**/*.{js,css,html,png,svg,ico,woff,woff2}"],
+    },
+    client: {
+      installPrompt: true,
+    },
+    devOptions: {
+      enabled: false,
+      suppressWarnings: true,
+      navigateFallback: "/",
+      type: "module",
+    },
+  },
 });
